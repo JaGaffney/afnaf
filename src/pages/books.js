40b2016698bf263e64993a5ff734fde1ea987ba2
JaGaffney <jagaffney661@msn.com>
2020-03-27 20:32:43 +1100

    testing svgs

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,10 +1,23 @@
 import React from "react"
 
+import SVG from "../images/svg"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 const Books = () => {
   return (
-    <div>
-      <h1>Coming soon</h1>
-    </div>
+    <Layout>
+      <SEO title="Coming Soon" />
+      <div>
+        <h1>Coming soon</h1>
+
+        <h3>Civ Schedule Manager</h3>
+        <div className="svg-container">
+          <SVG style={{ height: "20rem" }} />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
